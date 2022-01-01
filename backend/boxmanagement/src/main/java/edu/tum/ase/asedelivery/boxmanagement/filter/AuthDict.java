b5f5e017b22812933f8d17b49f57c92773d5a260
Joaquin <joaquin.murcia@estudiante.uam.es>
2022-01-01 12:03:41 +0100

    AuthFilter and map created

---
+++ backend/boxmanagement/src/main/java/edu/tum/ase/asedelivery/boxmanagement/filter/AuthDict.java
@@ -0,0 +1,13 @@
+package edu.tum.ase.asedelivery.boxmanagement.filter;
+
+import edu.tum.ase.asedelivery.asedeliverymodels.UserRole;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthDict extends HashMap<String, UserRole> {
+    /* public HashMap<String, UserRole>() {
+        this.putAll(Map.of("/boxes",UserRole.ROLE_DISPATCHER,))
+    } */
+}
--- backend/boxmanagement/src/main/java/edu/tum/ase/asedelivery/boxmanagement/filter/AuthFilter.java
+++ backend/boxmanagement/src/main/java/edu/tum/ase/asedelivery/boxmanagement/filter/AuthFilter.java
@@ -1,4 +1,21 @@
 package edu.tum.ase.asedelivery.boxmanagement.filter;
 
-public class AuthFilter {
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Component
+public class AuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+        ArrayList<String> sub =  new ArrayList<>(Arrays.asList(path.split("boxmanagement")));
+    }
 }
