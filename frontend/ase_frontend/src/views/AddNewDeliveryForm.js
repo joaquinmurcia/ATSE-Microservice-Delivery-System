4488942708f95a03fe2cf65065c5a70b24772ffb
Charles <charles.donven@tum.de>
2022-01-17 02:36:48 +0100

    upgrade add and edit form from delivery management

--- frontend/ase_frontend/src/views/AddNewDeliveryForm.js
+++ frontend/ase_frontend/src/views/AddNewDeliveryForm.js
@@ -1,11 +1,43 @@
 import Button from "@mui/material/Button";
 import {TextField, Typography, Paper, Container, MenuItem, Select, InputLabel, FormControl} from "@mui/material"
 import {useDispatch} from "react-redux";
+import React, {useState} from "react";
+import {addElement} from "./deliveriesSlice";
 
 
-const AddNewForm = () => {
+const AddNewDeliveryForm = () => {
 
     const dispatch = useDispatch();
+
+    const[elemTargetBox,setTargetBox] = useState("");
+    const[elemTargetCustomer,setTargetCustomer] = useState("");
+    const[elemResponsibleDriver,setResponsibleDriver] = useState("");
+    const[elemDeliveryStatus,setDeliveryStatus] = useState("");
+
+    const handleChangeTargetBox = (e) => {
+        setTargetBox(e.target.value);
+    }
+
+    const handleChangeTargetCustomer = (e) => {
+        setTargetCustomer(e.target.value);
+    }
+
+    const handleChangeResponsibleDriver = (e) => {
+        setResponsibleDriver(e.target.value);
+    }
+
+    const handleChangeDeliveryStatus = (e) => {
+        setDeliveryStatus(e.target.value);
+    }
+
+    function getElem() {
+        return {
+            targetBox:  elemTargetBox,
+            targetCustomer: elemTargetCustomer,
+            responsibleDriver: elemResponsibleDriver,
+            deliveryStatus: elemDeliveryStatus
+        }
+    }
 
     return (
         <Container>
@@ -13,26 +45,26 @@ const AddNewForm = () => {
                 <Typography component="h1" variant="h6" align="center" marginBottom={5}>
                     Add new Delivery
                 </Typography>
-                <TextField sx={{minWidth: 120, margin: 1}} size="small" name="targetBox" label="Target Box"/>
+                <TextField sx={{minWidth: 120, margin: 1}} size="small" name="targetBox" label="Target Box" value={elemTargetBox} onChange={handleChangeTargetBox}/>
                 <br/>
-                <TextField sx={{minWidth: 120, margin: 1}} size="small" name="targetCustomer" label="Target Customer"/>
+                <TextField sx={{minWidth: 120, margin: 1}} size="small" name="targetCustomer" label="Target Customer" value={elemTargetCustomer} onChange={handleChangeTargetCustomer}/>
                 <br/>
-                <TextField sx={{minWidth: 120, margin: 1}} size="small" name="responsibleDriver" label="Responsible Driver"/>
+                <TextField sx={{minWidth: 120, margin: 1}} size="small" name="responsibleDriver" label="Responsible Driver" value={elemResponsibleDriver} onChange={handleChangeResponsibleDriver}/>
                 <br/>
-                <FormControl sx={{minWidth: 120, margin: 1}}size="small">
-                <InputLabel id="selectStatus">Status</InputLabel>
-                <Select name="deliveryStatus" labelId="selectStatus" label="">
-                    <MenuItem value=""><em>None</em></MenuItem>
-                    <MenuItem value="open">Open</MenuItem>
-                    <MenuItem value="pickedUp">Picked Up</MenuItem>
-                    <MenuItem value="delivered">Delivered</MenuItem>
-                </Select>
+                <FormControl sx={{minWidth: 120, margin: 1}} size="small">
+                    <InputLabel id="selectStatus">Status</InputLabel>
+                    <Select name="deliveryStatus" labelId="selectStatus" label="" value={elemDeliveryStatus} onChange={handleChangeDeliveryStatus}>
+                        <MenuItem value=""><em>None</em></MenuItem>
+                        <MenuItem value="open">Open</MenuItem>
+                        <MenuItem value="pickedUp">Picked Up</MenuItem>
+                        <MenuItem value="delivered">Delivered</MenuItem>
+                    </Select>
                 </FormControl>
                 <br/>
-                <Button sx={{minWidth: 100, margin: 1}} variant="contained" size="small" color="success">Add</Button>
+                <Button sx={{minWidth: 100, margin: 1}} variant="contained" size="small" color="success" onClick={()=> dispatch(addElement(getElem()))}>Add</Button>
             </Paper>
         </Container>
     );
 }
 
-export default AddNewForm;
+export default AddNewDeliveryForm;
