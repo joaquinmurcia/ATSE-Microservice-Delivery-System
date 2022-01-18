7a1113fdb765c3ec9eaa36f77d36f0c997c209ec
Josef Kamysek <josef@kamysek.com>
2022-01-18 12:03:52 +0100

    - Disabled CSRF

--- backend/emailnotification/src/main/java/edu/tum/ase/asedelivery/emailnotification/config/SecurityConfig.java
+++ backend/emailnotification/src/main/java/edu/tum/ase/asedelivery/emailnotification/config/SecurityConfig.java
@@ -5,48 +5,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Autowired
-//    MongoUserDetailsService mongoUserDetailsService;
-//
-//    @Override
-//    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-//        builder.userDetailsService(mongoUserDetailsService);
-//    }
 
     @Autowired
     AuthRequestFilter authRequestFilter;
 
 
-// Http Config, Authentication Manager Bean Definition, and BcryptPasswordEncoder
-
+    // Http Config, Authentication Manager Bean Definition, and BcryptPasswordEncoder
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(authRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//                    .antMatchers("/**").authenticated()
-//                .and()
-//                .sessionManagement().disable();
+        http.addFilterBefore(authRequestFilter, UsernamePasswordAuthenticationFilter.class).
+                csrf().disable();
     }
 
     @Override
     @Bean
-// Define an authentication manager to execute authentication services
+    // Define an authentication manager to execute authentication services
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
     @Bean
-// Define an instance of Bcrypt for hashing passwords
+    // Define an instance of Bcrypt for hashing passwords
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
