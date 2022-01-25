8042970912f53a4525e9809fed9180e2f4cddc65
Joaquin <joaquin.murcia@estudiante.uam.es>
2022-01-25 12:23:20 +0100

    asedeliverymodels package, other modules can't use

--- backend/asedeliverymodels/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module asedeliverymodels {
-    requires org.mongodb.driver.core;
-    requires spring.data.commons;
-    requires spring.data.mongodb;
-    requires java.management;
-    requires spring.security.core;
-    requires lombok;
-    exports edu.tum.ase.asedelivery.asedeliverymodels;
-}
