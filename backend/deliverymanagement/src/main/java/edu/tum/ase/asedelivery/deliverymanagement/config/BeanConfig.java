f1dd9c217690d7dd00d0bc747fd9677bd127d12f
Josef Kamysek <josef@kamysek.com>
2022-02-11 11:50:55 +0100

    - Adjusted BeanConfig location

--- backend/deliverymanagement/src/main/java/edu/tum/ase/asedelivery/deliverymanagement/config/BeanConfig.java
+++ backend/deliverymanagement/src/main/java/edu/tum/ase/asedelivery/deliverymanagement/config/BeanConfig.java
@@ -1,4 +1,4 @@
-package edu.tum.ase.asedelivery.boxmanagement.config;
+package edu.tum.ase.asedelivery.deliverymanagement.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
